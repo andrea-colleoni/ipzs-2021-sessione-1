d420008aa5c32bd2b0f002dc6829742e097dd0cc
Andrea Colleoni <andrea@colleoni.info>
2021-08-06 11:28:28 +0200

    aggiunto progetto spring boot

---
+++ devops/eclipse/progetto-finale/src/test/java/it/ipzs/progettofinale/ProgettoFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ipzs.progettofinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
